6d9a9b0bb3430ecac9054b83f6e82778269c2e1b
AndriiKharchenk <levsha90@gmail.com>
2025-10-05 16:40:23 +0300

    added items and List

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-let shopingList = ["молоко, хлеб, яйца, мясо"];
+let shoppingList = ['молоко', 'хлеб', 'яйца', 'мясо'];
+
+function addItem(item) {
+  shoppingList.push(item);
+}
+
+function removeItem(item) {
+  for (let i = 0; i < shoppingList.length; i++) {
+    if (shoppingList[i] === item) {
+      shoppingList.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function showList() {
+  console.log('Список покупок:');
+  shoppingList.forEach((item, index) => {
+    console.log(index + 1 + '. ' + item);
+  });
+}
+
+
+addItem("сыр");
+showList();
+
+addItem("банаы");
+showList();
+
+removeItem("хлеб");
+showList();
