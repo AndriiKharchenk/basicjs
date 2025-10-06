f50fde51f3ae79161e33082cf2af80fe83c08fe4
AndriiKharchenk <levsha90@gmail.com>
2025-10-06 11:44:26 +0300

    fixed first parth, created loadUsers

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let shoppingList = ['молоко', 'хлеб', 'яйца', 'мясо'];
+const shoppingList = ['молоко', 'хлеб', 'яйца', 'мясо'];
 
-function addItem(item) {
+const addItem = (item) => {
   shoppingList.push(item);
 }
 
-function removeItem(item) {
+const removeItem = (item) => {
   for (let i = 0; i < shoppingList.length; i++) {
     if (shoppingList[i] === item) {
       shoppingList.splice(i, 1);
@@ -13,7 +13,7 @@ function removeItem(item) {
   }
 }
 
-function showList() {
+const showList = () => {
   console.log('Список покупок:');
   shoppingList.forEach((item, index) => {
     console.log(index + 1 + '. ' + item);
@@ -29,3 +29,20 @@ showList();
 
 removeItem("хлеб");
 showList();
+
+const loadUsers = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error(`Помилка HTTP: ${response.status}`);
+    }
+    const users = await response.json();
+    console.log('Список користувачів:');
+    console.table(users);
+  } catch (error) {
+    console.error('Помилка при завантажені користувачів:', error.message);
+  }
+};
+
+loadUsers();
+
